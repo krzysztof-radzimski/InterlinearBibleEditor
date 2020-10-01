e139de4b8331c0d4cc38716cf680a063191153b5
krzysztof-radzimski <krzysztof@radzimski.pl>
2020-10-01 23:20:35 +0200

    Add book identifiers

---
+++ src/Usfm.Identifiers/Attributes/NumberAttribute.cs
@@ -0,0 +1,18 @@
+﻿/*=====================================================================================
+	Author: (C)2009 - 2020 ITORG Krzysztof Radzimski
+	http://itorg.pl
+    License: MIT
+  ===================================================================================*/
+
+using System;
+
+namespace Usfm.Identifiers.Attributes {
+    [AttributeUsage(AttributeTargets.Field)]
+    public class NumberAttribute : Attribute {
+        public string Number { get; set; }
+        public NumberAttribute() { }
+        public NumberAttribute(string number) : this() {
+            Number = number;
+        }
+    }
+}
