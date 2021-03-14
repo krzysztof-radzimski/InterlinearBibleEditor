3099fb562ce5174a285a0ac22f33848f9651b222
krzysztof-radzimski <krzysztof@radzimski.pl>
2021-03-14 09:44:34 +0100

    add searching as get method

--- src/Church.WebApp/Controllers/SearchController.cs
+++ src/Church.WebApp/Controllers/SearchController.cs
@@ -27,7 +27,14 @@ namespace Church.WebApp.Controllers {
             _logger = logger;
         }
 
-        public IActionResult Index() {            
+        public IActionResult Index() {
+            var qs = Request.QueryString;
+            if (qs .IsNotNull() && qs.Value.IsNotNullOrEmpty() && qs.Value.Length > 9) {
+                //?text=ala+ma+kota
+                var queryString = Uri.UnescapeDataString(qs.Value);
+                var words = queryString.Replace("?text=","").Split('+', StringSplitOptions.RemoveEmptyEntries);
+                return _Search(words);
+            }
             return View();
         }
 
@@ -35,46 +42,49 @@ namespace Church.WebApp.Controllers {
         public IActionResult Index(string text) {
             if (!String.IsNullOrEmpty(text) && text.Length > 3) {
                 var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-                var view = new XPView(new UnitOfWork(), typeof(Verse));
-
-                var critera = String.Empty;
-                foreach (var word in words) {
-
-                    critera += $"Contains(Lower([Text]),'{word}')";
-
-                    if (word != words.Last()) {
-                        critera += " AND ";
-                    }
-                }
-
-                view.CriteriaString = critera;
-
-
-                view.Properties.Add(new ViewProperty("NumberOfBook", SortDirection.None, "[ParentChapter.ParentBook.NumberOfBook]", false, true));
-                view.Properties.Add(new ViewProperty("BookShortcut", SortDirection.None, "[ParentChapter.ParentBook.BookShortcut]", false, true));
-                view.Properties.Add(new ViewProperty("NumberOfChapter", SortDirection.None, "[ParentChapter.NumberOfChapter]", false, true));
-                view.Properties.Add(new ViewProperty("NumberOfVerse", SortDirection.None, "[NumberOfVerse]", false, true));
-                view.Properties.Add(new ViewProperty("VerseText", SortDirection.None, "[Text]", false, true));
-                view.Properties.Add(new ViewProperty("TranslationName", SortDirection.None, "[ParentChapter.ParentBook.ParentTranslation.Description]", false, true));
-                view.Properties.Add(new ViewProperty("Translation", SortDirection.None, "[ParentChapter.ParentBook.ParentTranslation.Name]", false, true));
-
-                var model = new SearchResultsModel(words);
-                foreach (ViewRecord record in view) {                    
-                        model.Add(new SearchItemModel() {
-                            Book = record["NumberOfBook"].ToInt(),
-                            BookShortcut = record["BookShortcut"].ToString(),
-                            Chapter = record["NumberOfChapter"].ToInt(),
-                            Verse = record["NumberOfVerse"].ToInt(),
-                            TranslationName = record["TranslationName"].ToString(),
-                            Translation = record["Translation"].ToString().Replace("'", ""),
-                            VerseText = record["VerseText"].ToString()
-                        });                 
-                }
-
-                return View(model);
+                return _Search(words);
             }
             return View();
+        }
+
+        private IActionResult _Search(string[] words) {
+            var view = new XPView(new UnitOfWork(), typeof(Verse));
+
+            var critera = String.Empty;
+            foreach (var word in words) {
+
+                critera += $"Contains(Lower([Text]),'{word}')";
+
+                if (word != words.Last()) {
+                    critera += " AND ";
+                }
+            }
+
+            view.CriteriaString = critera;
+
+
+            view.Properties.Add(new ViewProperty("NumberOfBook", SortDirection.None, "[ParentChapter.ParentBook.NumberOfBook]", false, true));
+            view.Properties.Add(new ViewProperty("BookShortcut", SortDirection.None, "[ParentChapter.ParentBook.BookShortcut]", false, true));
+            view.Properties.Add(new ViewProperty("NumberOfChapter", SortDirection.None, "[ParentChapter.NumberOfChapter]", false, true));
+            view.Properties.Add(new ViewProperty("NumberOfVerse", SortDirection.None, "[NumberOfVerse]", false, true));
+            view.Properties.Add(new ViewProperty("VerseText", SortDirection.None, "[Text]", false, true));
+            view.Properties.Add(new ViewProperty("TranslationName", SortDirection.None, "[ParentChapter.ParentBook.ParentTranslation.Description]", false, true));
+            view.Properties.Add(new ViewProperty("Translation", SortDirection.None, "[ParentChapter.ParentBook.ParentTranslation.Name]", false, true));
+
+            var model = new SearchResultsModel(words);
+            foreach (ViewRecord record in view) {
+                model.Add(new SearchItemModel() {
+                    Book = record["NumberOfBook"].ToInt(),
+                    BookShortcut = record["BookShortcut"].ToString(),
+                    Chapter = record["NumberOfChapter"].ToInt(),
+                    Verse = record["NumberOfVerse"].ToInt(),
+                    TranslationName = record["TranslationName"].ToString(),
+                    Translation = record["Translation"].ToString().Replace("'", ""),
+                    VerseText = record["VerseText"].ToString()
+                });
+            }
+
+            return View(model);
         }
     }
 
