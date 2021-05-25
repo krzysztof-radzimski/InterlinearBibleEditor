a096da891dc69acdae919eba6bb7f0564b698bf6
krzysztof-radzimski <krzysztof@radzimski.pl>
2021-05-25 22:45:49 +0200

    Add grammar codes and strongs pages

---
+++ src/Church.WebApp/Controllers/GrammarsCodeController.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Xpo;
+using IBE.Common.Extensions;
+using IBE.Data.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Church.WebApp.Controllers {
+    public class GrammarsCodeController : Controller {
+        public IActionResult Index() {
+            var qs = Request.QueryString;
+            if (qs.IsNotNull() && qs.Value.IsNotNullOrEmpty() && qs.Value.Length > 3) {
+                var value = qs.Value;
+                if (value.Contains("&")) {
+                    value = value.Substring(0, value.IndexOf("&"));
+                }
+                var id = value.Replace("?id=", "").Trim();
+                var grammarCode = new XPQuery<GrammarCode>(new UnitOfWork()).Where(x => x.GrammarCodeVariant1 == id).FirstOrDefault();
+                if (grammarCode.IsNotNull()) {
+                    return View(grammarCode);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ src/Church.WebApp/Controllers/StrongsCodeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IBE.Common.Extensions;
+using DevExpress.Xpo;
+using IBE.Data.Model;
+
+namespace Church.WebApp.Controllers {
+    public class StrongsCodeController : Controller {
+        private readonly ILogger<StrongsCodeController> _logger;
+
+        public StrongsCodeController(ILogger<StrongsCodeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            var qs = Request.QueryString;
+            if (qs.IsNotNull() && qs.Value.IsNotNullOrEmpty() && qs.Value.Length > 3) {
+                var value = qs.Value;
+                if (value.Contains("&")) {
+                    value = value.Substring(0, value.IndexOf("&"));
+                }
+                var id = value.ToLower().Replace("?id=", "").Trim().ToInt();
+                var strongCode = new XPQuery<StrongCode>(new UnitOfWork()).Where(x => x.Code == id && x.Lang == Language.Greek).FirstOrDefault();
+                if (strongCode.IsNotNull()) {
+                    return View(strongCode);
+                }
+            }
+            return View();
+        }
+    }
+}
