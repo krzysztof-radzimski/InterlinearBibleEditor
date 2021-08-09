ebf806632e5c316151337175859fb8f268290d0e
ABC PRO Sp. z o.o <67048819+abcpro-pl@users.noreply.github.com>
2021-08-09 12:42:17 +0200

    Add sharing article tags

--- src/Church.WebApp/Controllers/ArticleController.cs
+++ src/Church.WebApp/Controllers/ArticleController.cs
@@ -15,8 +15,12 @@ using DevExpress.Xpo;
 using IBE.Common.Extensions;
 using IBE.Data.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Church.WebApp.Controllers {
     public class ArticleController : Controller {
@@ -27,10 +31,10 @@ namespace Church.WebApp.Controllers {
         }
 
         public IActionResult Index() {
-            var qs = Request.QueryString;            
+            var qs = Request.QueryString;
             if (qs.IsNotNull() && qs.Value.IsNotNullOrEmpty() && qs.Value.Length > 3) {
                 var value = qs.Value;
-                if (value.Contains("&")){
+                if (value.Contains("&")) {
                     value = value.Substring(0, value.IndexOf("&"));
                 }
                 var id = value.ToLower().Replace("?id=", "").Trim().ToInt();
@@ -40,6 +44,46 @@ namespace Church.WebApp.Controllers {
                 }
             }
             return View();
+        }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArticleImageController : Controller {
+        private readonly IConfiguration Configuration;
+        public ArticleImageController(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get() {
+            var qs = Request.QueryString;
+
+            if (qs.IsNotNull() && qs.Value.IsNotNullOrEmpty()) {
+                var value = qs.Value;
+                if (value.Contains("&")) {
+                    value = value.Substring(0, value.IndexOf("&"));
+                }
+                var queryString = Uri.UnescapeDataString(value).RemoveAny("?id=");
+                var stream = await GetImage(queryString);
+                if (stream.IsNull()) { return NotFound(); }
+                return File(stream, "image/jpg", "article.jpg");
+            }
+
+            return NotFound();
+        }
+        private async Task<Stream> GetImage(string queryString) {
+            var paramsTable = queryString.Split(',');
+            if (paramsTable.Length > 0) {
+                var articleId = paramsTable[0].ToInt();
+                if (articleId > 0) {
+                    var uow = new UnitOfWork();
+                    var article = new XPQuery<Article>(uow).Where(x => x.Oid == articleId).FirstOrDefault();
+                    if (article.IsNotNull()) {
+                        return new MemoryStream(article.AuthorPicture);
+                    }
+                }
+            }
+            return default;
         }
     }
 }
