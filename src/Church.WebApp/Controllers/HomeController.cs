4d577e6ef0fee861859bd95610f090135a2ba304
ABC PRO Sp. z o.o <67048819+abcpro-pl@users.noreply.github.com>
2021-09-14 15:56:20 +0200

    Add "about bible" page

--- src/Church.WebApp/Controllers/HomeController.cs
+++ src/Church.WebApp/Controllers/HomeController.cs
@@ -71,6 +71,10 @@ namespace Church.WebApp.Controllers {
             return View();
         }
 
+        public IActionResult AboutBible() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
