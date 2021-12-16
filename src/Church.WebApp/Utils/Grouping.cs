bf978882962bb2c17c9bb6dc8b22712b9b1c30a8
ABC PRO Sp. z o.o <67048819+abcpro-pl@users.noreply.github.com>
2021-12-16 11:09:19 +0100

    Changes in bible combos

---
+++ src/Church.WebApp/Utils/Grouping.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Church.WebApp.Utils {
+    public class Grouping<TKey, TElement> : IGrouping<TKey, TElement> {
+
+        readonly List<TElement> elements;
+
+        public Grouping(IGrouping<TKey, TElement> grouping) {
+            if (grouping == null)
+                throw new ArgumentNullException("grouping");
+            Key = grouping.Key;
+            elements = grouping.ToList();
+        }
+        public Grouping(TKey key, IEnumerable<TElement> items) {
+            if (key == null)
+                throw new ArgumentNullException("key");
+            if (items == null)
+                throw new ArgumentNullException("items");
+            Key = key;
+            elements = items.ToList();
+        }
+        public TKey Key { get; private set; }
+
+        public IEnumerator<TElement> GetEnumerator() {
+            return this.elements.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+
+        public int Count => elements != null ? elements.Count : 0;
+    }
+}
