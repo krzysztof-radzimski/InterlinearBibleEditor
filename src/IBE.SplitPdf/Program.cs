7a8e91b8ea40f92b151fa5aa178e77e89cdac46f
Krzysztof Radzimski <42403863+krzysztof-radzimski@users.noreply.github.com>
2022-01-29 19:51:53 +0100

    IBE.SplitPDF

---
+++ src/IBE.SplitPdf/Program.cs
@@ -0,0 +1,11 @@
+using Aspose.Pdf;
+using System.IO;
+
+if (args.Length == 2) {
+    var path = args[0];
+    var startPage = Convert.ToInt32(args[1]);
+
+    if (File.Exists(path) && startPage > 0) { 
+    
+    }
+}
