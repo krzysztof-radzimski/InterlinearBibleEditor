5e8c5fca4357690dfe8dd8d7729228ceb38c3a17
ABC PRO Sp. z o.o <67048819+abcpro-pl@users.noreply.github.com>
2022-01-29 20:18:11 +0100

    IBE.SPliPdf

--- src/IBE.SplitPdf/Program.cs
+++ src/IBE.SplitPdf/Program.cs
@@ -1,11 +1,15 @@
-using Aspose.Pdf;
-using System.IO;
+using Aspose.Pdf.Facades;
 
 if (args.Length == 2) {
     var path = args[0];
     var startPage = Convert.ToInt32(args[1]);
 
-    if (File.Exists(path) && startPage > 0) { 
-    
+    if (File.Exists(path) && startPage > 0) {
+        new Aspose.Pdf.License().SetLicense("../../../../../db/Aspose.Total.lic");
+
+        var pdfEditor = new PdfFileEditor();
+        var outputFilePath = Path.Combine(Path.GetDirectoryName(path), $"{Path.GetFileNameWithoutExtension(path)}.{startPage}.{Path.GetExtension(path)}");
+        //var outputFilePath = @"C:\Users\krzysztof.radzimski\Documents\export.pdf";
+        pdfEditor.SplitToEnd(path, startPage, outputFilePath);
     }
 }
