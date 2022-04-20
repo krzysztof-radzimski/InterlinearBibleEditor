9d59a25dff142924fc20bdc74d0db76d25c2e909
ABC PRO Sp. z o.o <67048819+abcpro-pl@users.noreply.github.com>
2022-04-20 19:21:43 +0200

    footnotes bug fixes

---
+++ src/Church.WebApp/Models/FootnoteInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace Church.WebApp.Models {
+    public class FootnoteInfo {
+        public int Index { get; set; }
+        public string Title { get; set; }
+        public string Html { get; set; }
+    }
+}
